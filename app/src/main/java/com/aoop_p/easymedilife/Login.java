b10d68f29796e34c1b9258dfd1eac8c6c023400b
Nobab khan <75307851+Nobabkh@users.noreply.github.com>
2022-07-15 08:55:09 +0600

    changed connectivity

--- app/src/main/java/com/aoop_p/easymedilife/Login.java
+++ app/src/main/java/com/aoop_p/easymedilife/Login.java
@@ -1,12 +1,39 @@
 package com.aoop_p.easymedilife;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.aoop_p.easymedilife.dbmanager.DBManager;
+
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.NClob;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.sql.Struct;
+import java.util.Map;
+import java.util.Properties;
 
 public class Login extends AppCompatActivity {
     DBManager db;
@@ -16,32 +43,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         db = new DBManager();
-        checkconnectionandupdate();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                checkconnectionandupdate();
+            }
+        };
+        r.run();
+        //checkconnectionandupdate();
+
     }
+
     void checkconnectionandupdate()
     {
-        if(!db.connect())
-        {
-            if(!activitychanged) {
-                setContentView(R.layout.activity_changedburl);
-                activitychanged = true;
-            }
-            Button save = findViewById(R.id.button2);
-            save.setOnClickListener(view -> {
-                EditText url = findViewById(R.id.editTextTextPersonName);
-                String dburl = url.getText().toString();
-                db = new DBManager(dburl);
-                if(db.connect())
-                {
-                    setContentView(R.layout.activity_login);
-                    activitychanged = false;
-                }
-                else
-                {
-                    checkconnectionandupdate();
-                }
-            });
-        }
+        Toast.makeText(this, db.connect(), Toast.LENGTH_SHORT).show();
     }
 }
